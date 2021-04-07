22fbd6fd688b3608220aac9af1dbf5e6d98c2e5a
Alexandre Tannus <alexandretannus@gmail.com>
2021-04-07 16:06:05 -0300

    Herança

---
+++ 04-05-heranca/index.js
@@ -0,0 +1,31 @@
+class Animal {
+    sex
+    
+    constructor(props) {
+        this.sex = props.sex
+    }
+}
+
+class Human extends Animal {
+    hungry = true
+    name
+    age
+
+    constructor(props) {
+        super(props)
+        this.name = props.name
+        this.age = props.age
+    }
+
+    eat(){
+        this.hungry = false
+    }
+}
+
+const person = new Human({
+    name: 'Alexandre',
+    age: 33,
+    sex: 'male'
+})
+
+console.log(person);
