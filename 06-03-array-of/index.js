3de4e51e2215b5d37694627bb226fcb4fecd341d
Alexandre Tannus <alexandretannus@gmail.com>
2021-04-13 08:34:43 -0300

    Criando arrays do zero com Array.of

---
+++ 06-03-array-of/index.js
@@ -0,0 +1,15 @@
+// Criando arrays - forma tradicional
+const array = [1, 2, 3]
+console.log(array);
+
+// Criando arrays - new Array
+const newArray = new Array(2, 4, 6)
+console.log(newArray);
+
+// Define array com tamanho fixo e todos os valores vazios. Iteração não funciona
+const array2 = new Array(6)
+console.log(array2);
+
+// Criando array com apenas um elemento com Array.of
+const arrayOf = Array.of(6)
+console.log(arrayOf);
