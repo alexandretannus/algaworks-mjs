60e17f6c78f963c3d10956579adf3d94c749d7c4
Alexandre Tannus <alexandretannus@gmail.com>
2021-04-14 10:35:25 -0300

    Introdução ao Typescript

---
+++ 09-01-intro-typescript/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const me = {
+    name: 'Alexandre',
+    age: 33
+};
+exports.default = me;
---
+++ 09-01-intro-typescript/index.ts
@@ -0,0 +1,6 @@
+const me = {
+    name: 'Alexandre',
+    age: 33
+}
+
+export default me
