c88aa680c0eeef8bd6468256a75c454920e3d4b2
Alexandre Tannus <alexandretannus@gmail.com>
2021-04-14 10:48:18 -0300

    Tipagem de variáveis

---
+++ 09-02-tipagem-variaveis/index.ts
@@ -0,0 +1,15 @@
+// nomeVariavel:tipoVariavel
+const personName: string = 'Alexandre'
+const personAge: number = 33
+const personAlive: boolean = true
+const personContacts: string[] = [
+    'teste@gmail.com',
+    'eu@eu.br'
+]
+
+// Múltiplos tipos
+const personContacts2: (string | number)[] = [
+    'teste@gmail.com',
+    'eu@eu.br',
+    999999
+]
